1200f2be1d136b1887068b925e6e546d627cfdbe
Yuri_gomes_062 <yurioficial892@gmail.com>
2025-12-03 11:45:51 -0300

    update

--- MVP_SOLID.ts
+++ MVP_SOLID.ts
@@ -1,1 +1,28 @@
- asv 
+type cargo = "TI" | "gestor" | "RH" | "contador" // etc...
+type usuario = "gestor" | "colaborador"// etc...
+
+class Funcionario{
+    name:string;
+    position:string;
+    department:string
+    permission:string
+    constructor(name:string,position:string,department:string,permission:string){
+        this.name = name;
+        this.position = position;
+        this.department = department;
+        this.permission = permission;
+    }
+}
+class Avaliacao_Desempenho_Nine_box {
+    public usuario: string; // classe 1
+    public pontuacao: number; // classe 2
+    public cargo: string;// classe 1
+    public quadrante?: string;// classe 2
+    
+    constructor(usuario: string, cargo: string, pontuacao: number, quadrante: string) {
+        this.usuario = usuario;
+        this.cargo = cargo;
+        this.pontuacao = pontuacao;
+        this.quadrante = quadrante;
+    }
+}
