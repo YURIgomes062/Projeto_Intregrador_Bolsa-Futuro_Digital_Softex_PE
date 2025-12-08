90949c8ad2f5dfd25bcae38908c706c76b0c84df
Yuri_gomes_062 <yurioficial892@gmail.com>
2025-12-08 01:57:04 -0300

    Update version for SOLID

--- MVP_SOLID.ts
+++ MVP_SOLID.ts
@@ -1,28 +1,38 @@
 type cargo = "TI" | "gestor" | "RH" | "contador" // etc...
 type usuario = "gestor" | "colaborador"// etc...
 
-class Funcionario{
+
+interface Icolaborador{
+    department:string;
     name:string;
-    position:string;
-    department:string
-    permission:string
-    constructor(name:string,position:string,department:string,permission:string){
+    permission:usuario;
+    position:cargo;
+    
+}
+
+class Funcionario implements Icolaborador{
+    name:string;        // nome
+    position:cargo;   // cargo
+    department:string; // departamento
+    permission:usuario; // nível de permissão do usuario
+    constructor(name:string,position:cargo,department:string,permission:usuario){
         this.name = name;
         this.position = position;
         this.department = department;
         this.permission = permission;
     }
 }
-class Avaliacao_Desempenho_Nine_box {
-    public usuario: string; // classe 1
-    public pontuacao: number; // classe 2
-    public cargo: string;// classe 1
-    public quadrante?: string;// classe 2
-    
-    constructor(usuario: string, cargo: string, pontuacao: number, quadrante: string) {
-        this.usuario = usuario;
-        this.cargo = cargo;
-        this.pontuacao = pontuacao;
-        this.quadrante = quadrante;
+// concluido
+//-----------------------------= Lógica classe concluida =------------------------------------\\
+// n concluido
+
+
+function Avaliacao_Desempenho_Nine_box(user:Funcionario){
+    if(user.permission == "gestor"){
+        // Perguntas a serem feitas para o gestor sobre o colaborador...
+        
+    }
+    else if(user.permission == "colaborador"){
+        // perguntas a serem respondidas pelo colaborador para auto avaliação...
     }
 }
